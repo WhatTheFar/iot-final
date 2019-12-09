ae41d0039ec5520de01a0791d0d8e3bebb164d17
WhatTheFar <jakpat.m@gmail.com>
2019-12-09 17:09:34 +0700

    scripts(face): trainmodel.sh

---
+++ face-recognition/train_model.sh
@@ -0,0 +1,10 @@
+echo "### Extracting embeddings"
+python extract_embeddings.py --dataset dataset --embeddings output/embeddings.pickle --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7
+
+echo
+echo
+echo
+echo
+
+echo "### Training model"
+python train_model.py --embeddings output/embeddings.pickle --recognizer output/recognizer.pickle --le output/le.pickle
